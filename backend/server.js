030ebe607d23592c0030c166c75e0c495276cba4
smackeem <smackeem@outlook.com>
2023-12-15 10:12:23 -0500

    Server is created and running.

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const cors = require('cors');
+const morgan = require('morgan');
+require("dotenv").config();
+require('./config/database');
+
+const {PORT} = process.env;
+
+const express = require("express");
+
+const app = express();
+
+app.get('/', (req, res)=> {
+    res.send("Hello world");
+});
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+app.use(cors());
+app.use(morgan("dev"));
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
