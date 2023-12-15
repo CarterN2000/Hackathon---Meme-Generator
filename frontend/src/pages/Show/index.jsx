5a03b3f5c42b29d4c261261474f8688d90b43cad
smackeem <smackeem@outlook.com>
2023-12-15 15:33:33 -0500

    grab id from params in show page.

--- frontend/src/pages/Show/index.jsx
+++ frontend/src/pages/Show/index.jsx
@@ -5,11 +5,12 @@ import { useParams } from "react-router";
 import './Show.css'
 
 export default function Show() {
+    const {id} = useParams()
 const [meme, setMeme] = useState({})
     const [isLoading, setIsLoading] = useState(true)
 
     const handleRequest = async() => {
-        const data = await showMeme()
+        const data = await showMeme(id)
         console.log(data)
         setMeme(data)
         setIsLoading(false)
